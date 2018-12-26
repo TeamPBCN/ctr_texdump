14433b7a9a315e7ab981afac32fde5d47aa89292
littoma <littoma@163.com>
2018-12-26 18:05:55 +0800

    Version 1.

---
+++ src/texdump/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+using CommandLine.Text;
+using System.Drawing;
+using CTR;
+
+namespace texdump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options)) { return; }
+            if (options.LastParserState != null)
+                if (options.LastParserState.Errors.Count > 0) { return; }
+
+            if (!File.Exists(options.Input))
+            {
+                Console.WriteLine($"{options.Input} doesn't exists.");
+                return;
+            }
+
+            CTRTexture texture = new CTRTexture();
+            texture.Size = options.Size;
+            texture.Format = options.Format;
+
+            var fs = File.OpenRead(options.Input);
+            BinaryReader reader = new BinaryReader(fs);
+            fs.Position = options.Position;
+            texture.ImageData = reader.ReadBytes(options.Length);
+
+            Bitmap bmp = TextureUtil.DecodeTexture(texture);
+            Bitmap output = new Bitmap(bmp.Width, bmp.Height);
+            Graphics gfx = Graphics.FromImage(output);
+            gfx.DrawRectangle(new Pen(Color.FromArgb(0x00FFFFFF)), 0, 0, output.Width, output.Height);
+            gfx.DrawImageUnscaled(bmp, 0, 0);
+
+            if (!string.IsNullOrEmpty(options.Output))
+                output.Save(options.Output);
+        }
+    }
+
+    class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Set input file.")]
+        public string Input { get; set; }
+        [Option('o', "output", HelpText = "Set output file.")]
+        public string Output { get; set; }
+        [Option('f', "format", Required = true, HelpText = "Set pixel format.")]
+        public CTR.TextureFormat Format { get; set; }
+        [Option('p', "position", DefaultValue = 0, HelpText = "Set texture data position.")]
+        public int Position { get; set; }
+        [Option('l', "length", HelpText = "Set texture data length.")]
+        public int Length { get; set; }
+        [Option('w', "width", HelpText = "set image width.")]
+        public int Width { get; set; }
+        [Option('h', "height", HelpText = "Set image height.")]
+        public int Height { get; set; }
+
+        public Size Size { get { return new Size(Width, Height); } }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+        [HelpOption("help")]
+        public string GetHelp()
+        {
+            return HelpText.AutoBuild(this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
