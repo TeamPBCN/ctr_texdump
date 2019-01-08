ee03373b9a2c37535d41a8b8b666aea69238ab9a
littoma <littoma@163.com>
2019-01-08 17:04:00 +0800

    Add texdumpcore.

--- src/texdumpcore/Program.cs
+++ src/texdumpcore/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using CommandLine;
 using CommandLine.Text;
-using System.Drawing;
 using CTR;
 
 namespace texdump
